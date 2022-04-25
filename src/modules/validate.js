107687f774eb57961a9b1d4437de301aeac047fa
elena-nikulina <info@webstar-support.com>
2022-04-25 22:01:23 +0700

    Сделала основное задание к уроку 21

--- src/modules/validate.js
+++ src/modules/validate.js
@@ -4,63 +4,102 @@ const validate = () => {
     let calcCount = document.querySelector('.calc-count');
     let calcDay = document.querySelector('.calc-day');
     let form1 = document.querySelector('#form1');
+    let form2 = document.querySelector('#form2');
+    let form3 = document.querySelector('#form3');
     let form1Name = document.querySelector('#form1-name');
     let form1Email = document.querySelector('#form1-email');
     let form1Phone = document.querySelector('#form1-phone');
-
-    //console.log(form1Name);
-    //console.log(form1Email);
-    //console.log(form1Phone);
+    let form2Email = document.querySelector('#form2-email');
+    let form2Message = document.querySelector('#form2-message');
+    let form3Phone = document.querySelector('#form3-phone');
+    let form3Email = document.querySelector('#form3-email');
 
     calcSquare.addEventListener('input', (e) => {
-        console.log(e.target.value);
+
         e.target.value = e.target.value.replace(/[^\d+]/g, "");
 
     });
     calcCount.addEventListener('input', (e) => {
-        console.log(e.target.value);
+
         e.target.value = e.target.value.replace(/[^\d+]/g, "");
 
     });
 
     calcDay.addEventListener('input', (e) => {
-        console.log(e.target.value);
+
         e.target.value = e.target.value.replace(/[^\d+]/g, "");
 
     });
 
-    /*form1Name.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace((/\d+/), "");
-    });
-
-    form1Email.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace((/\d+/), "");
-    });
-
-    form1Phone.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace((/\d+/), "");
-    });*/
-
     form1.addEventListener('submit', (e) => {
         e.preventDefault();
         let isError = false;
-
-        /*if (!/[^а-яА-Я]/g.test(form1Name.value)) {
-            alert('В инпуте только кириллица');
-        }*/
    
-        if (/[^a-zA-Z0-9+\_\@\-\!\*\~\'\.]+/g.test(form1Email.value)) {
+        if (/[a-zA-Z0-9+\_\@\-\!\*\~\'\.]+/g.test(form1Email.value) && form1Email.value !== "") {
             alert('В инпуте только латиница, цифры и спецсимволы');
+        } else {
+            isError = true;
         }
 
-        if (/[\d\-()]/g.test(form1Phone.value)) {
+        if (/[\d\-()]/g.test(form1Phone.value) && form1Phone.value !== "") {
             alert('В инпуте только цифры, дефис и скобки');
+        } else {
+            isError = true;
         }
 
         if (!isError) {
-            //alert("Данные отправлены!");
+            alert("Данные отправлены!");
         }
         
+    });
+
+    form2.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let isError2 = false;
+
+        if (/[a-zA-Z0-9+\_\@\-\!\*\~\'\.]+/g.test(form2Email.value) && form2Email.value !== "") {
+            alert('В инпуте только латиница, цифры и спецсимволы');
+        } else {
+            isError2 = true;
+        }
+
+        if (/[\d\-()]/g.test(form2Phone.value) && form2Phone.value !== "") {
+            alert('В инпуте только цифры, дефис и скобки');
+        } else {
+            isError2 = true;
+        }
+
+        if (/[а-яА-Я]+\-\s/g.test(form2Message.value)) {
+            alert('В инпуте только кириллица, дефис и пробел');
+        }
+
+        if (!isError2) {
+            alert("Данные отправлены!");
+        }
+        
+    });
+
+    form3.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let isError3 = false;
+
+        if (/[\d\-()]/g.test(form3Phone.value) && form3Phone.value !== "") {
+            alert('В инпуте только цифры, дефис и скобки');
+        } else {
+            isError3 = true;
+        }
+
+        if (/[a-zA-Z0-9+\_\@\-\!\*\~\'\.]+/g.test(form3Email.value) && form3Email.value !== "") {
+            alert('В инпуте только латиница, цифры и спецсимволы');
+        } else {
+            isError3 = true;
+        }
+
+        if (!isError3) {
+            alert("Данные отправлены!");
+        }
+
+
     });
 };
 
