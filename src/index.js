b55757ee76ceef5941d013e75533e461963ac994
elena-nikulina <info@webstar-support.com>
2022-04-30 11:16:27 +0700

    Где-то ошибка, не нашла

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import validate from './modules/validate';
 import tabs from './modules/tabs';
 import menu from './modules/menu';
 import modal from './modules/modal';
+import slider from './modules/slider';
 
 timer('23 april 2022');//one
 menu();
 modal();
 validate();
 tabs();
+slider();
