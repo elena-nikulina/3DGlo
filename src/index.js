2ba55a4311ecd5dcaba41b73bbb1af90db44da03
elena-nikulina <info@webstar-support.com>
2022-05-01 00:36:31 +0700

    Сделала основное задание к уроку 24

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import tabs from './modules/tabs';
 import menu from './modules/menu';
 import modal from './modules/modal';
 import slider from './modules/slider';
+import calc from './modules/calc';
 
 timer('23 april 2022');//one
 menu();
@@ -14,3 +15,4 @@ modal();
 validate();
 tabs();
 slider();
+calc(100);
